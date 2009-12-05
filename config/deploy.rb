a052555a2c093b05fad1e7bc9ffe2ff6fb310079
Aaron Bieber <airborne@orion.(none)>
2009-12-05 15:09:30 -0500

    Updated the deploy file to remove the temporary multi-environments. Now only deploying to "hydra."

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,4 @@
 set :application, "workshops"
 set :repository, "git://github.com/aaronbieber/workshops.git"
-
-task :new do
-	set :domain, "diagramwar.com"
-	set :deploy_to, "/var/www/artphotoworkshops.com/workshops"
-end
-
-task :current do
-	set :domain, "artphotoworkshops.com"
-	set :deploy_to, "/var/www/artphotoworkshops.com/workshops"
-end
+set :domain, "artphotoworkshops.com"
+set :deploy_to, "/var/www/artphotoworkshops.com/workshops"
