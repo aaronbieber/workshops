03b03e991ddd25c7e2b45052865412ff2fc3f39b
Aaron Bieber <aaron@aaronbieber.com>
2010-03-22 20:41:21 -0400

    Added unit testing stuff.

--- test/unit/workshop_test.rb
+++ test/unit/workshop_test.rb
@@ -1,7 +1,16 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class WorkshopTest < Test::Unit::TestCase
-  fixtures :workshops
+#  fixtures :workshops
+
+  context "a workshop"
+    setup do
+    end
+
+    should "not save when empty"
+      assert true
+    end
+  end
 
   # Replace this with your real tests.
   def test_truth
